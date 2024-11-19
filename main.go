01a1909a993a3d12e1ea191185da84b31ff95cef
justinjest <jest.cayaban@gmail.com>
2024-11-19 10:43:55 +0900

    Using staticcheck

--- main.go
+++ main.go
@@ -24,6 +24,10 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unusued() {
+	// this does nothing and is pontless
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
